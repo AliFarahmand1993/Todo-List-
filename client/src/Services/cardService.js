442025accf1a4e0ceea2480becf4a3cce90be055
Ali Farahmand <alifarahmand1993@gmail.com>
2023-04-04 21:29:19 +0430

    update url

--- client/src/Services/cardService.js
+++ client/src/Services/cardService.js
@@ -53,7 +53,7 @@ import {
 	updateStartDueDatesOfCard,
 } from '../Redux/Slices/listSlice';
 
-const baseUrl = 'http://localhost:3001/card';
+const baseUrl = 'https://todo-list-server-theta.vercel.app/card';
 let submitCall = Promise.resolve();
 
 export const getCard = async (cardId, listId, boardId, dispatch) => {
